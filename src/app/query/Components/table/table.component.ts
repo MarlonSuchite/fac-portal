35640dc224fb076b18238726854c9110946ebf0b
dvelasquez-2021489 <dvelasquez-2021489@kinal.edu.gt>
2023-09-14 22:19:11 -0600

    Se agrego paginación

--- src/app/query/Components/table/table.component.ts
+++ src/app/query/Components/table/table.component.ts
@@ -1,13 +1,32 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Peticiones } from '../../interfaces/peticiones';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
   displayedColumns: string[] = ['identifier', 'date', 'entity', 'user', 'operation'];
-  @Input() dataSource = [
-    
-  ];;
+  @Input() dataSource: Peticiones[] = [];
+  tableDataSource = new MatTableDataSource<Peticiones>(this.dataSource);
+
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+
+  //Detectar cambiar a la hora de buscar
+  ngOnChanges(changes: SimpleChanges): void {
+    this.tableDataSource = new MatTableDataSource<Peticiones>(this.dataSource);
+    this.tableDataSource.paginator = this.paginator;
+  }
+  
+  ngAfterViewInit() {
+    this.tableDataSource.paginator = this.paginator;
+    console.log(this.tableDataSource.paginator)
+  }
+
+  ngOnInit(): void {
+    this.tableDataSource.paginator = this.paginator;
+  }
+
 }
